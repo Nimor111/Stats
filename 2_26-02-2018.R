09e468a80608eb283eb5680e93abeaa76c6c0cfd
Nimor111 <georgi.bojinov@hotmail.com>
2018-02-26 19:01:57 +0200

    Add problems from 26.02

---
+++ 2_26-02-2018.R
@@ -0,0 +1,65 @@
+library(MASS)
+
+attach(survey)
+
+# Factor of data - like an enum
+# use str(survey$Smoke)
+# levels(survey$Smoke)
+
+# problem 1
+# P(A) = #R / #N
+
+probability_of_regular_smoker = sum(Smoke == 'Regul', na.rm=T) / nrow(survey)
+
+t = table(Smoke)
+
+probability_of_regular_smoker2 = sum(t['Regul']) / nrow(survey)
+p = prop.table(t)
+probability_of_regular_smoker3 = sum(p['Regul'])
+
+t2 = table(Smoke, Sex)
+p2 = prop.table(t2)
+probability_of_regular_male_smoker = p2['Regul', 'Male']
+
+# P(B) = #M & R / #M
+# probability_of_male_being_smoker
+# sum of all cols will be 1
+p3 = prop.table(t2, 2)
+
+# P(C) = #M & R / #R
+# sum of all rows will be 1
+p4 = prop.table(t2, 1)
+
+# problem 2
+y = factor(Smoke, levels=levels(Smoke)[c(2, 3, 4, 1)])
+t = table(y)
+# pie(t, col=c('green', 'red', 'blue' ,'orange'), main='SMOKERS')
+# barplot(t, col=c('green', 'red', 'blue', 'orange'))
+
+t2 = table(Sex, y)
+barplot(t2, col=c('green', 'red'), legend=levels(Sex), beside=T)
+
+t2alt = table(y, Sex)
+barplot(t2alt, col=c('green', 'red', 'blue', 'orange'), legend=levels(Smoke), beside=T)
+
+t2prop = prop.table(table(Sex, y), 2)
+barplot(t2prop, col=c('green', 'red'), legend=levels(Sex), beside=T)
+
+# problem 3
+# average 
+# x1...xn, average is sum(xi)/n
+average_height = mean(Height, na.rm = T)
+median_height = median(Height, na.rm=T)
+
+s = summary(Height)
+boxplot(Height, horizontal = T)
+
+boxplot(Height~Sex, horizontal=T)
+# boxplot(Height[Sex=='Male'], Height[Sex=='Female'], horizontal = T)
+
+# removes 0.1 from both sides of the interval, if we have outlier values it will
+# effectively ignore them
+
+# type h in console for more information about it
+h = hist(Height)
+mean_without_outliers = mean(Height, na.rm=T, trim=0.1)
